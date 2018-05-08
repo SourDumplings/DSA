2627c3b50e4e5287715fafbb69748c54dcf723aa
SourDumplings <changzheng300@foxmail.com>
2018-05-08 15:50:00 +0800

    增加了斐波那契类和函数

---
+++ Cpp/Algorithms/Fib.h
@@ -0,0 +1,100 @@
+/*
+ @Date    : 2018-05-08 14:55:13
+ @Author  : 酸饺子 (changzheng300@foxmail.com)
+ @Link    : https://github.com/SourDumplings
+ @Version : $Id$
+*/
+
+/*
+斐波那契数生成类
+ */
+
+#ifndef FIB_H
+#define FIB_H
+
+#include <cstdio>
+#include <stdexcept>
+
+namespace CZ
+{
+    class Fib
+    {
+    public:
+        // 初始化为不小于n的最小斐波那契项，默认为第0项
+        Fib(long long n): lastItem(1), thisItem(0)
+        {
+            while (thisItem < n)
+            {
+                next();
+            }
+        }
+        Fib(): Fib(0) {}
+
+        long long get()
+        {
+            // 获取当前斐波那契项
+            return thisItem;
+        }
+        long long next()
+        {
+            // 获取并转至下一个斐波那契项
+            thisItem += lastItem;
+            lastItem = thisItem - lastItem;
+            return thisItem;
+        }
+        long long prev()
+        {
+            // 获取并转至前一个斐波那契项
+            try
+            {
+                if (thisItem > 0)
+                {
+                    lastItem = thisItem - lastItem;
+                    thisItem -= lastItem;
+                }
+                else
+                    throw "prev failure";
+            }
+            catch (const char *errMsg)
+            {
+                printf("%s\n", errMsg);
+                throw std::runtime_error("cannot prev any more.");
+            }
+            return thisItem;
+        }
+
+        // 获取
+    private:
+        long long lastItem, thisItem; // lastItem = fib(k-1), thisItem = fib(k)。均为int型，很快就会溢出。
+    };
+
+    // 得到斐波那契数列的第n项，2个版本的方法
+    long long get_fib(int n, unsigned version = 0)
+    {
+        long long lastItem = 1, thisItem = 0;
+        switch (version)
+        {
+            case 0:
+            {
+                // 迭代法，O(n)
+                while (n--)
+                {
+                    thisItem += lastItem;
+                    lastItem = thisItem - lastItem;
+                }
+                return thisItem;
+            }
+            case 1:
+            {
+                // 二分递归法, O(2^n)
+                return (n < 2) ? n : (get_fib(n - 1, 1) + get_fib(n - 2, 1));
+            }
+        }
+    }
+} // CZ
+
+
+
+#endif // FIB_H
+
+
--- Cpp/Algorithms/test.cpp
+++ Cpp/Algorithms/test.cpp
@@ -12,10 +12,12 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <stdexcept>
 
 #include "..\Vector\Vector.h"
 #include "Search.h"
 #include "Binary_search.h"
+#include "Fib.h"
 
 using namespace std;
 using namespace CZ;
@@ -23,38 +25,61 @@ using namespace CZ;
 int main(int argc, char const *argv[])
 {
     // 测试查找函数模板
-    Vector<int> v1({1, 5, 7, 88, -3});
-    auto r1 = Search(v1.begin(), v1.end(), 5), r2 = Search(v1.begin() + 3, v1.end(), -3);
-    auto r3 = Search(v1.begin(), v1.end() - 1, -3), r4 = Search(v1.begin(), v1.end(), -3);
-    printf("The search result for r1: pos %d\n", r1 - v1.begin());
-    printf("The search result for r2: pos %d\n", r2 - v1.begin());
-    printf("The search result for r3: pos %d\n", r3 - v1.begin());
-    printf("The search result for r4: pos %d\n", r4 - v1.begin());
-    auto r10 = Search(v1.begin(), v1.end(), 10);
-    if (r10 == v1.end())
-    {
-        printf("r10 is fail\n");
-    }
+    // Vector<int> v1({1, 5, 7, 88, -3});
+    // auto r1 = Search(v1.begin(), v1.end(), 5), r2 = Search(v1.begin() + 3, v1.end(), -3);
+    // auto r3 = Search(v1.begin(), v1.end() - 1, -3), r4 = Search(v1.begin(), v1.end(), -3);
+    // printf("The search result for r1: pos %d\n", r1 - v1.begin());
+    // printf("The search result for r2: pos %d\n", r2 - v1.begin());
+    // printf("The search result for r3: pos %d\n", r3 - v1.begin());
+    // printf("The search result for r4: pos %d\n", r4 - v1.begin());
+    // auto r10 = Search(v1.begin(), v1.end(), 10);
+    // if (r10 == v1.end())
+    // {
+    //     printf("r10 is fail\n");
+    // }
 
-    Vector<string> vs1({"abc", "def", "cde"});
-    auto rs1 = Search(vs1.begin(), vs1.end(), string("a"), [] (const string & s1, const string & s2)
-    { return s1[0] == s2[0]; });
-    printf("The search result for rs1: pos %d\n", rs1 - vs1.begin());
+    // Vector<string> vs1({"abc", "def", "cde"});
+    // auto rs1 = Search(vs1.begin(), vs1.end(), string("a"), [] (const string & s1, const string & s2)
+    // { return s1[0] == s2[0]; });
+    // printf("The search result for rs1: pos %d\n", rs1 - vs1.begin());
 
-    int a1[7] {1, 3, 5, 7, 9, 11};
-    auto r5 = Binary_search(a1 + 0, a1 + 6, 5);
-    printf("The search result for r5: pos %d\n", r5 - a1);
+    // int a1[7] {1, 3, 5, 7, 9, 11};
+    // auto r5 = Binary_search(a1 + 0, a1 + 6, 5);
+    // printf("The search result for r5: pos %d\n", r5 - a1);
 
-    auto r6 = Binary_search(a1 + 0, a1 + 6, 3, SEARCH_VERSION3);
-    printf("The search result for r6: pos %d\n", r6 - a1);
+    // auto r6 = Binary_search(a1 + 0, a1 + 6, 3, SEARCH_VERSION3);
+    // printf("The search result for r6: pos %d\n", r6 - a1);
 
-    auto r7 = Binary_search(a1 + 0, a1 + 6, 8),
-        r8 = Binary_search(a1 + 0, a1 + 6, 8, SEARCH_VERSION2),
-        r9 = Binary_search(a1 + 0, a1 + 6, 8, SEARCH_VERSION3);
-    printf("The search result for r7: pos %d\n", r7 - a1);
-    printf("The search result for r8: pos %d\n", r8 - a1);
-    printf("The search result for r9: pos %d\n", r9 - a1);
+    // auto r7 = Binary_search(a1 + 0, a1 + 6, 8),
+    //     r8 = Binary_search(a1 + 0, a1 + 6, 8, SEARCH_VERSION2),
+    //     r9 = Binary_search(a1 + 0, a1 + 6, 8, SEARCH_VERSION3);
+    // printf("The search result for r7: pos %d\n", r7 - a1);
+    // printf("The search result for r8: pos %d\n", r8 - a1);
+    // printf("The search result for r9: pos %d\n", r9 - a1);
 
+    // 测试斐波那契
+    // Fib f1, f2(2), f3(11);
+    // printf("f1 = %lld, f2 = %lld, f3 = %lld\n", f1.get(), f2.get(), f3.get());
+    // f1.next();
+    // printf("f1 = %lld\n", f1.get());
+    // f2.next(); f2.next();
+    // printf("f2 = %lld\n", f2.get());
+    // for (unsigned i = 0; i < 10; ++i)
+    // {
+    //     try
+    //     {
+    //         f3.prev();
+    //         printf("f3 = %lld\n", f3.get());
+    //     }
+    //     catch (const runtime_error &err)
+    //     {
+    //         printf("%s\n", err.what());
+    //         break;
+    //     }
+    // }
+
+    // printf("fib[0]0 = %lld, fib[0]1 = %lld, fib[5]0 = %lld, fib[3]1 = %lld\n",
+    //     get_fib(0, 0), get_fib(0, 1), get_fib(5, 0), get_fib(3, 1));
 
 
     return 0;
