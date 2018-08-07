d5a624f2ad7b99898d044e45ca343d018b2bd87a
SourDumplings <changzheng300@foxmail.com>
2018-08-07 22:43:00 +0800

    增加了素数表类，采用埃式筛法生成素数表来进行素数相关操作

---
+++ PrimeTable/PrimeTable.h
@@ -0,0 +1,152 @@
+/*
+ @Date    : 2018-08-06 22:52:59
+ @Author  : 酸饺子 (changzheng300@foxmail.com)
+ @Link    : https://github.com/SourDumplings
+ @Version : $Id$
+*/
+
+/*
+素数表类
+用埃式筛法生成素数表
+ */
+
+#ifndef PRIME_TABLE_H
+#define PRIME_TABLE_H
+
+#include "../Vector/Vector.h"
+#include <stdexcept>
+#include <cstdio>
+#include <iostream>
+
+namespace CZ
+{
+    class PrimeTable
+    {
+    public:
+        using Rank = unsigned;
+
+        // 构造存放最大素数不超过upperLimit_的素数表
+        PrimeTable(Rank upperLimit_);
+        // 素数表中素数的个数
+        Rank size() const;
+        // 素数表的上界
+        Rank upper_limit() const;
+        void rebuild(Rank upperLimit_);
+        // 返回第n个素数
+        Rank get_prime(Rank n) const;
+        // 判断素数
+        bool is_prime(Rank num);
+
+        // 输出信息
+        void print_info(const char *name = "") const;
+
+    private:
+        // 素数表大小，即素数的上界（所有存的素数都不大于它）
+        Rank _upperLimit;
+        // 素数的个数
+        Rank _size;
+        Vector<bool> _table;
+
+        // 利用埃氏筛法构建素数表
+        void _build_table();
+    };
+
+        // 构造存放最大素数不超过upperLimit_的素数表
+    PrimeTable::PrimeTable(PrimeTable::Rank upperLimit_): _upperLimit(upperLimit_),
+        _size(0), _table(_upperLimit+1) { _build_table(); }
+
+    // 素数表中素数的个数
+    PrimeTable::Rank PrimeTable::size() const { return _size; }
+    // 素数表的上界
+    PrimeTable::Rank PrimeTable::upper_limit() const { return _upperLimit; }
+    void PrimeTable::rebuild(PrimeTable::Rank upperLimit_)
+    {
+        _upperLimit = upperLimit_;
+        _size = 0;
+        _table.resize(_upperLimit+1);
+        _build_table();
+        return;
+    }
+    // 返回第n个素数
+    PrimeTable::Rank PrimeTable::get_prime(PrimeTable::Rank n) const
+    {
+        if (n > _size)
+        {
+            printf("Error from PrimeTable get_prime: this table doesn't have so many primes\n");
+            throw std::exception();
+        }
+        PrimeTable::Rank ret;
+        for (PrimeTable::Rank i = 2, count = 0; i <= _upperLimit; ++i)
+        {
+            if (_table[i])
+            {
+                ++count;
+            }
+            if (count == n)
+            {
+                ret = i;
+                break;
+            }
+        }
+        return ret;
+    }
+    // 判断素数
+    bool PrimeTable::is_prime(PrimeTable::Rank num)
+    {
+        if (num > _upperLimit)
+        {
+            printf("Error from PrimeTable get_prime: this number is too large\n");
+            throw std::exception();
+        }
+        return _table[num];
+    }
+
+    // 输出信息
+    void PrimeTable::print_info(const char *name) const
+    {
+        printf("for PrimeTable %s:\n", name);
+        std::cout << "it contains " << _size << " prime numbers, no greater than " << _upperLimit
+            << std::endl;
+        printf("including:");
+        for (Rank i = 2; i <= _upperLimit; ++i)
+        {
+            if (_table[i])
+            {
+                std::cout << " " << i;
+            }
+        }
+        printf("\n\n");
+        return;
+    }
+
+    void PrimeTable::_build_table()
+    {
+        if (_upperLimit < 2)
+        {
+            printf("Error from PrimeTable _build_table: _upperLimit is too small\n");
+            throw std::exception();
+        }
+
+        _table[0] = _table[1] = false;
+        for (PrimeTable::Rank i = 2; i <= _upperLimit; ++i)
+        {
+            _table[i] = true;
+        }
+
+        for (PrimeTable::Rank i = 2; i <= _upperLimit; ++i)
+        {
+            if (_table[i])
+            {
+                for (PrimeTable::Rank j = i << 1; j <= _upperLimit; j += i)
+                {
+                    _table[j] = false;
+                }
+                ++_size;
+            }
+        }
+        return;
+    }
+} // CZ
+
+#endif // PRIME_TABLE_H
+
---
+++ PrimeTable/test.cpp
@@ -0,0 +1,57 @@
+/*
+ @Date    : 2018-08-07 22:12:12
+ @Author  : 酸饺子 (changzheng300@foxmail.com)
+ @Link    : https://github.com/SourDumplings
+ @Version : $Id$
+*/
+
+/*
+测试素数表
+ */
+
+#include <iostream>
+#include <cstdio>
+#include "PrimeTable.h"
+
+using namespace std;
+using namespace CZ;
+
+int main(int argc, char const *argv[])
+{
+    PrimeTable p1(10), p2(100), p3(1000);
+    p1.print_info("p1");
+    p2.print_info("p2");
+    p3.print_info("p3");
+
+    if (p1.is_prime(2))
+    {
+        printf("2 is prime number\n");
+    }
+    if (p1.is_prime(9))
+    {
+        printf("9 is prime number\n");
+    }
+    // if (p1.is_prime(20))
+    // {
+    //     printf("20 is prime number\n");
+    // }
+    if (p2.is_prime(23))
+    {
+        printf("23 is prime number\n");
+    }
+    if (p3.is_prime(227))
+    {
+        printf("227 is prime number\n");
+    }
+    if (p3.is_prime(111))
+    {
+        printf("111 is prime number\n");
+    }
+
+    cout << p1.get_prime(3) << endl;
+    cout << p2.get_prime(10) << endl;
+    cout << p3.get_prime(103) << endl;
+    // cout << p1.get_prime(7) << endl;
+    return 0;
+}
+
