d1fe0b1c6222fe7fac5a93de6e0c76fed278d1ea
Chang Zheng <changzheng300@foxmail.com>
2019-09-22 21:33:45 +0800

    修正了树的后序遍历bug

--- Cpp/Tree/Tree_traversal.h
+++ Cpp/Tree/Tree_traversal.h
@@ -1,9 +1,10 @@
 /*
- @Date    : 2018-05-26 16:27:22
- @Author  : 酸饺子 (changzheng300@foxmail.com)
- @Link    : https://github.com/SourDumplings
- @Version : $Id$
-*/
+ * @Autor: SourDumplings
+ * @Date: 2019-09-21 22:54:58
+ * @Link: https://github.com/SourDumplings/
+ * @Email: changzheng300@foxmail.com
+ * @Description: 
+ */
 
 /*
 树的遍历算法，对于多叉树只实现了递归版
@@ -17,58 +18,57 @@
 
 namespace CZ
 {
-    template <typename T>
-    template <typename F>
-    void Tree<T>::pre_order_traversal(TreeNode<T> *root, const F &visit)
+template <typename T>
+template <typename F>
+void Tree<T>::pre_order_traversal(TreeNode<T> *root, const F &visit)
+{
+    if (root)
     {
-        if (root)
+        visit(root->data());
+        for (auto &c : root->children())
         {
-            visit(root->data());
-            for (auto &c : root->children())
-            {
-                pre_order_traversal(c);
-            }
+            pre_order_traversal(c);
         }
-        return;
     }
+    return;
+}
 
-    template <typename T>
-    template <typename F>
-    void Tree<T>::post_order_traversal(TreeNode<T> *root, const F &visit)
+template <typename T>
+template <typename F>
+void Tree<T>::post_order_traversal(TreeNode<T> *root, const F &visit)
+{
+    if (root)
     {
-        if (root)
+        for (auto &c : root->children())
         {
-            for (auto &c : root->children())
-            {
-                pre_order_traversal(c);
-            }
-            visit(root->data());
+            post_order_traversal(c);
         }
-        return;
+        visit(root->data());
     }
+    return;
+}
 
-    template <typename T>
-    template <typename F>
-    void Tree<T>::level_order_traversal(TreeNode<T> *root, const F &visit)
+template <typename T>
+template <typename F>
+void Tree<T>::level_order_traversal(TreeNode<T> *root, const F &visit)
+{
+    Queue<TreeNode<T> *> q;
+    q.push(root);
+    while (!q.empty())
     {
-        Queue<TreeNode<T>*> q;
-        q.push(root);
-        while (!q.empty())
+        TreeNode<T> *node = q.front();
+        q.pop();
+        if (node)
         {
-            TreeNode<T> *node = q.front(); q.pop();
-            if (node)
+            visit(node->data());
+            for (auto &c : node->children())
             {
-                visit(node->data());
-                for (auto &c : node->children())
-                {
-                    q.push(c);
-                }
+                q.push(c);
             }
         }
-        return;
     }
-} // CZ
+    return;
+}
+} // namespace CZ
 
 #endif // TREE_TRAVERSAL_H
-
-
