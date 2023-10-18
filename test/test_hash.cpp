8d3a012ca19acfb8c70cc92e53491a9f5b0466e7
SourDumplings <changzheng300@foxmail.com>
2023-10-18 18:46:01 +0800

    9.4.4：使用 PRId64 宏解决跨平台输出问题

--- test/test_hash.cpp
+++ test/test_hash.cpp
@@ -11,6 +11,7 @@
 #include "Vector/Vector.h"
 #include "List/List.h"
 #include "Set/HashSet.h"
+#include <cinttypes>
 
 using namespace CZ;
 using namespace std;
@@ -18,17 +19,17 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     bool b1 = true, b2 = false;
-    printf("Hash<bool>()(b1) = %llu, Hash<bool>()(b2) = %llu\n", Hash<bool>()(b1), Hash<bool>()(b2));
+    printf("Hash<bool>()(b1) = %" PRId64 ", Hash<bool>()(b2) = %" PRId64 "\n", Hash<bool>()(b1), Hash<bool>()(b2));
     int i1 = 1234, i2 = 0, i3 = -1, i4 = 1000000, i5 = INT32_MAX, i6 = INT32_MIN;
-    printf("Hash<int>()(i1) = %llu, Hash<int>()(i2) = %llu, Hash<int>()(i3) = %llu, Hash<int>()(i4) = %llu, Hash<int>()(i5) = %llu, Hash<int>()(i6) = %llu\n"
+    printf("Hash<int>()(i1) = %" PRId64 ", Hash<int>()(i2) = %" PRId64 ", Hash<int>()(i3) = %" PRId64 ", Hash<int>()(i4) = %" PRId64 ", Hash<int>()(i5) = %" PRId64 ", Hash<int>()(i6) = %" PRId64 "\n"
         , Hash<int>()(i1), Hash<int>()(i2), Hash<int>()(i3), Hash<int>()(i4), Hash<int>()(i5), Hash<int>()(i6));
     float f1 = 0, f2 = 123.99, f3 = 10.5e6;
-    printf("Hash<float>()(f1) = %llu, Hash<float>()(f2) = %llu, Hash<float>()(f3) = %llu\n", Hash<float>()(f1), Hash<float>()(f2), Hash<float>()(f3));
+    printf("Hash<float>()(f1) = %" PRId64 ", Hash<float>()(f2) = %" PRId64 ", Hash<float>()(f3) = %" PRId64 "\n", Hash<float>()(f1), Hash<float>()(f2), Hash<float>()(f3));
     double d1 = 0, d2 = 44.4e10, d3 = 1.88e-99;
-    printf("Hash<double>()(d1) = %llu, Hash<double>()(d2) = %llu, Hash<double>()(d3) = %llu\n"
+    printf("Hash<double>()(d1) = %" PRId64 ", Hash<double>()(d2) = %" PRId64 ", Hash<double>()(d3) = %" PRId64 "\n"
         , Hash<double>()(d1), Hash<double>()(d2), Hash<double>()(d3));
     char c1 = 'a', c2 = 'j';
-    printf("Hash<char>()(c1) = %llu, Hash<char>()(c2) = %llu\n", Hash<char>()(c1), Hash<char>()(c2));
+    printf("Hash<char>()(c1) = %" PRId64 ", Hash<char>()(c2) = %" PRId64 "\n", Hash<char>()(c1), Hash<char>()(c2));
 
     Vector<int> v1({ 5, 3, 10, 2, 4, 6, 2, 3, 7 }), v2({ 5, 3, 10, 3, 4, 6, 7 });
     cout << v1.hash() << endl;
@@ -41,7 +42,7 @@ int main(int argc, char const *argv[])
     cout << s2.hash() << endl;
 
     int *p = nullptr;
-    printf("Hash(p) = %llu\n", Hash<int*>()(p));
+    printf("Hash(p) = %" PRId64 "\n", Hash<int*>()(p));
 
     return 0;
 }
