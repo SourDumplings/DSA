00dc694f8613c318657245311f7fb450f8a67e01
SourDumplings <changzheng300@foxmail.com>
2023-10-25 16:56:50 +0800

    10.1.2：增加了 test_hello_world

---
+++ test/test_hello_world.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file test.cpp
+ * @author  SourDumplings (changzheng300@foxmail.com)
+ * @brief 单元测试程序
+ * @version 1.0.0
+ * @date 2023-10-25
+ *
+ * @copyright Copyright (c) 2023 SourDumplings
+ *
+ */
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World\n");
+
+    return 0;
+}
